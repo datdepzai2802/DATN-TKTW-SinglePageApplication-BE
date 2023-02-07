520d0bd324db5c62c9585e6145c34671d5abb8f0
ngotiendat <datmau20@gmail.com>
2023-02-07 14:31:35 +0700

    first API

--- src/v1/routers/user.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signupUser } from "../controllers/user.controllers";
-
-const router = express.Router();
-
-router.post("/signup", signupUser);
-
-export default router;
--- src/v1/utils/validator.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-export const productValidate = (data) => {
-  const productSchema = Joi.object({
-    name: Joi.string().lowercase().required(),
-    price: Joi.number().required(),
-  });
-  return productSchema.validate(data);
-};
