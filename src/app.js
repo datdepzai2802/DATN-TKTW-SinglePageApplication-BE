227e06bc78bcc9dc3deaf861d21162f37274915e
trandinhphuc281 <trandinhphuc281@gmail.com>
2023-02-16 22:31:36 +0700

    #1 CRUD-category

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ import morgan from "morgan";
 import "dotenv/config";
 import router from "./v1/routers/index.router";
 import createError from "http-errors";
+
+import routerCategory from "./v1/routers/categories/categories.router";
 //
 const app = express();
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 //middlewaer
 app.use(express.json());
 app.use(cors());
@@ -15,6 +17,7 @@ app.use(morgan("dev"));
 
 // router
 app.use("/api/v1", router);
+app.use("/api/v1", routerCategory);
 
 //
 app.use((req, res, next) => {
@@ -28,12 +31,13 @@ app.use((err, req, res, next) => {
 });
 
 //connect mongoose
-mongoose.connect(process.env.MONGGODB_Link, (error) => {
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/Pro220_DATN", (error) => {
   if (error) return console.log("Connect Database False!");
   console.log("Connect Database successfuly!");
 });
 
 //create server
-app.listen(PORT, () => {
-  console.log(`server running ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server running ${process.env.PORT}`);
 });
