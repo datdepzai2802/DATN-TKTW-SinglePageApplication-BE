a98ee42f6facfa098a60f7d395ff44c015be73f5
datdevelop2802 <79825535+ngotiendat28022k2@users.noreply.github.com>
2023-03-21 10:45:03 +0700

    #25 - feature/uploadfiles

--- src/v1/routers/index.router.js
+++ src/v1/routers/index.router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import auth from "./auth/index.router";
+import routerUpload from "./upload/upload.router";
 import routerCategory from "./categories/categories.router";
 import routerProduct from "./products/products.router";
 import routerUser from "./user/user.router";
@@ -9,5 +10,6 @@ router.use("", auth);
 router.use("", routerCategory);
 router.use("", routerProduct);
 router.use("", routerUser);
+router.use("", routerUpload);
 
 export default router;
