5cdc9e5518b2ddbf68e6f5904bbba6bf853f45da
datdevelop2802 <79825535+ngotiendat28022k2@users.noreply.github.com>
2023-03-27 12:19:44 +0700

    Update env.js

--- env.js
+++ env.js
@@ -1,9 +1,10 @@
 // PORT = 9090
 // MONGGO_URL = mongodb://localhost:27017/Api-BookOnline
-// MONGGO_ASLAT = "mongodb+srv://datn_booksShop:kvVg0PyY8Ic89aTO@cluster0.4atn5u5.mongodb.net/test"
+// MONGGO_ASLAT = "mongodb+srv://datn_booksShop:kvVg0PyY8Ic89aTO@cluster0.4atn5u5.mongodb.net/?retryWrites=true&w=majority"
 // # ACCOUT SEND GMAIL
 // TKSENDGMAIL="njkaonro@gmail.com"
 // PASSWORDSENDMAIL="wlpfbqyirwyruhui"
 // #  secretKey
 // ACCESS_TOKEN_SECRET = "amenAccess"
 // REFRESH_TOKEN_SECRET = "amenRefresh"
+// pas = kvVg0PyY8Ic89aTO
