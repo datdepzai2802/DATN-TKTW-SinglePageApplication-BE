84e637d9aab1b4c579641cc108a4a102f977fe46
datdevelop2802 <79825535+ngotiendat28022k2@users.noreply.github.com>
2023-03-27 12:31:36 +0700

    Delete src/v1/routers/upload directory

--- src/v1/routers/upload/upload.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-import Upload from "../../controllers/upload/upload.controller";
-import upload from "../../helppers/filehelpers";
-// router
-// router.post("/image", Upload.uploadImage);
-
-
-export default router;
