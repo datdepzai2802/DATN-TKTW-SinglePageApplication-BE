fd0413695f9a0c8a2ea2cc77519397026a1929f4
datdevelop2802 <79825535+ngotiendat28022k2@users.noreply.github.com>
2023-03-27 12:31:06 +0700

    Update index.router.js

--- src/v1/routers/index.router.js
+++ src/v1/routers/index.router.js
@@ -1,6 +1,5 @@
 import express from "express";
 import auth from "./auth/index.router";
-import routerUpload from "./upload/upload.router";
 
 import routerCategory from "./categories/categories.router";
 import routerUser from "./user/user.router";
@@ -13,9 +12,10 @@ const router = express.Router();
 
 router.use("", auth);
 router.use("", routerUser);
-// router.use("/upload", routerUpload);
 router.use("", routerFormbook);
 router.use("", routerPuslishing);
 router.use("", routerAuthor);
+router.use("", routerProduct);
+router.use("", routerCategory);
 
 export default router;
