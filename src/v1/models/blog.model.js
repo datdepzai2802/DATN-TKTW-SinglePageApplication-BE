6ead209f36f643a19dc0e7aff797ff140c01f0c2
tuananh-lc <anhdhtph16661@fpt.edu.vn>
2023-04-05 00:00:42 +0700

    #43 - Update BE part 2

--- src/v1/models/blog.model.js
+++ src/v1/models/blog.model.js
@@ -2,10 +2,10 @@ import mongoose, { ObjectId } from "mongoose";
 
 const blogSchema = mongoose.Schema({
   title: {
-    type: string,
+    type: String,
   },
   html: {
-    type: string,
+    type: String,
   },
   isHidden: {
     type: Boolean,
