f1395657bb6ab84d2e46c1ef1533eb1b50659da4
datdevelop2802 <79825535+ngotiendat28022k2@users.noreply.github.com>
2023-04-07 19:57:47 +0700

    Create product.model.js

--- src/v1/models/product.model.js
+++ src/v1/models/product.model.js
@@ -1,47 +1,54 @@
-import mongoose, { ObjectId } from "mongoose";
+  import mongoose, { ObjectId } from "mongoose";
 
-const productSchema = mongoose.Schema({
-  view: {
-    type: Number,
-  },
-  description: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
-  price: {
-    type: Number,
-  },
-  purchases: {
-    type: String,
-  },
-  images: {
-    type: Array,
-  },
-  sale: {
-    type: Number,
-  },
-  categories_id: {
-    type: String,
-  },
-  quantity: {
-    type: Number,
-  },
-  isHidden: {
-    type: Boolean,
-  },
-  publishing_hous_id: {
-    type: ObjectId,
-    ref: "Publishing",
-  },
-  form_book_id: {
-    type: ObjectId,
-    ref: "Formbook",
-  },
-  author_id: {
-    type: ObjectId,
-    ref: "Author",
-  },
-});
-export default mongoose.model("Product", productSchema);
+  const productSchema = mongoose.Schema({
+    view: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+    purchases: {
+      type: String,
+    },
+    productImage: {
+      type: Array,
+    },
+    previewImage: {
+      type: Array,
+    },
+    sale: {
+      type: Number,
+    },
+    quantity: {
+      type: Number,
+    },
+    isHidden: {
+      type: Boolean,
+    },
+    categorieId: {
+      type: String,
+    },
+    publishingHousId: {
+      type: ObjectId,
+      ref: "Publishing",
+    },
+    formBookId: {
+      type: ObjectId,
+      ref: "Formbook",
+    },
+    authorId: {
+      type: ObjectId,
+      ref: "Author",
+    },
+    supplieresId: {
+      type: ObjectId,
+      ref: "Supplieres",
+    },
+  });
+  export default mongoose.model("Product", productSchema);
